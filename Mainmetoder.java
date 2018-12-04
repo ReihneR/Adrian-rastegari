0da49bdbae53f0f61b580e666a024cfaee5873a9
Adrian <Adrianrastegari@yahoo.se>
2018-12-04 19:07:26 +0100

    jag glömde lägga den i rätt fil så jag kunde inte commita min progress men det här är vad jag har gjort så länge

---
+++ Mainmetoder.java
@@ -0,0 +1,14 @@
+package uppgiftv48;
+
+public class Mainmetoder {
+
+	public static void main(String[] args) {
+
+			System.out.println(metoder.volume(8));
+		
+			System.out.println(metoder.reverse("hussien sugger kuk"));
+			
+	}
+
+	
+}
---
+++ metoder.java
@@ -0,0 +1,35 @@
+package uppgiftv48;
+
+public class metoder {
+
+	
+		 public static double volume(double radius) {
+			 
+			double volume = 4 * Math.PI * Math.pow(radius, 3);
+			return volume;
+		 }
+		 
+		public static String reverse(String str) {
+			 String reverse = "";
+			 for(char i : str.toCharArray()) {
+				 reverse = i + reverse;
+				 
+				
+			 }
+			 return reverse;
+			                                          
+		 }
+		public static int count(String str, char c) {
+			int räkna = 0;
+			for (char bokstav : namn.toCharArray()) {
+
+				System.out.println("Bokstav: " + bokstav + " = " + c + " ?");
+				if(bokstav == c) {
+					räkna++;
+			
+			
+		}
+
+	}
+
+
