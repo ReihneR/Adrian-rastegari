554bee9c644407d9434f73641891d5f0a1e6c966
Adrian <Adrianrastegari@yahoo.se>
2019-02-21 13:58:18 +0100

    Update Gissatal.java

--- v46/Gissatal.java
+++ v46/Gissatal.java
@@ -6,54 +6,25 @@ public class Gissatal {
 		Scanner input = new Scanner(System.in);
 
 		System.out.println("välj ett tal mellan 1 - 20");
-		int a = (int)(Math.random()*20+1);
+		int a = (int) (Math.random() * 20 + 1);
 		int b = 0;
-		
-		while(b != a) {
-		b =	input.nextInt();
-		
-		if(a>b){
-			
-			System.out.println("Mitt tal är större");
-			
-			
-			
-		}
-		else if(a<b) {
-			System.out.println("mitt tal är lägre");
-		}
-		
-		else if(a == b) {
-			System.out.println("Du har valt rätt nummer GRATTIS!!");
-		}
+		boolean again ;
+
+		while (b != a) {
+			b = input.nextInt();
+
+			if (a > b) {
+
+				System.out.println("Mitt tal är större");
+
+			} else if (a < b) {
+				System.out.println("mitt tal är lägre");
+			}
+
+			else if (a == b) {
+				System.out.println("Du har valt rätt nummer GRATTIS!!");
+			}
 		}
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
