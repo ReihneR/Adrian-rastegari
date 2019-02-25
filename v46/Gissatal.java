7fbfe92c3cd9430da2d29cfb2a53b290263bf00b
Adrian <Adrianrastegari@yahoo.se>
2019-02-25 10:57:10 +0100

    Update Gissatal.java

--- v46/Gissatal.java
+++ v46/Gissatal.java
@@ -4,25 +4,44 @@ public class Gissatal {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-
-		System.out.println("välj ett tal mellan 1 - 20");
-		int a = (int) (Math.random() * 20 + 1);
+		// här frågar du att man ska välja ett tal mellan 1 till 20
+		System.out.println("välj ett tal mellan 1 - 10");
+		int a = (int) (Math.random() * 10 + 1);
 		int b = 0;
-		boolean again ;
+		boolean again = true;
+		int antal = 0;
+		while (again) {
 
-		while (b != a) {
-			b = input.nextInt();
+			while (b != a) {
+				antal = 0;
+				b = input.nextInt();
 
-			if (a > b) {
+				if (a > b) {
+					antal= antal +1;
+					System.out.println("Mitt tal är större");
 
-				System.out.println("Mitt tal är större");
+				} else if (a < b) {
+					antal= antal +1;
+					System.out.println("mitt tal är lägre");
+				}
 
-			} else if (a < b) {
-				System.out.println("mitt tal är lägre");
+				else if (a == b) {
+					antal= antal +1;
+					System.out.println("Du har valt rätt nummer GRATTIS!!, det tog " + antal + " försök");
+				}
 			}
+			System.out.println("vill du köra igen");
+			String fake = input.nextLine();
+			String fraga = input.nextLine();
+			if (fraga.equals("ja")) {
+				again = true;
+				a = (int) (Math.random() * 10 + 1);
+				System.out.println("välj ett tal mellan 1 - 10");
+				antal = 0;
 
-			else if (a == b) {
-				System.out.println("Du har valt rätt nummer GRATTIS!!");
+			} else {
+				System.out.println("okej hejdå");
+				break;
 			}
 		}
 	}
